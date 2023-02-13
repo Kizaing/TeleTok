31ef42b8288f1a403f8b5dd9eaa234da3acac7c9
Chris Plaatjes <kizaing@gmail.com>
2023-02-13 16:34:22 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using Telegram.Bot;
+
+namespace TeleTok
+{
+    class TeleTok
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json", true)
+                .Build();
+
+            var token = config.GetSection("TeleTokConf:token").Value;
+
+            var botClient = new TelegramBotClient(token);
+
+            var me = await botClient.GetMeAsync();
+
+            Console.WriteLine($"Hello world! I am user {me.Id} and my name is {me.FirstName}.");
+        }
+    }
+}
