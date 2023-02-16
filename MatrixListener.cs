2083e8279f9cc30bf73e00eece81dfdf88991564
Chris Plaatjes <kizaing@gmail.com>
2023-02-16 10:52:46 -0500

    Created initial matrix listener

---
+++ MatrixListener.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Configuration;
+using Matrix.Sdk;
+
+namespace TeleTok
+{
+    public class MatrixListener
+    {
+        public void RunListener()
+        {
+            
+        }
+    }
+}
